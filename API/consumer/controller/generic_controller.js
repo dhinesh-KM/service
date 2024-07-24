69285edbc8a9c69edfd4eb9cc56b83ddfabdd63a
dhinesh-KM <dhineshmuthu2002@gmail.com>
2024-07-24 16:33:45 +0530

    updated genric ctrlr

--- API/consumer/controller/generic_controller.js
+++ API/consumer/controller/generic_controller.js
@@ -4,7 +4,6 @@ const logger = require('../logger')
 const post = (method) => {
     return async (req, res, next) => {
         const data = req.body
-        console.log("::",req.user)
         if (req.user != undefined)
             data.cofferid = req.user.coffer_id
         if (Object.keys(req.params).length > 0)
