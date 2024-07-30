8141c1d4ee9edaddcebdd1390763f5eb4b7e3f08
dhinesh-KM <dhineshmuthu2002@gmail.com>
2024-07-30 20:34:45 +0530

    config changed

--- API/document/src/middleware/redis.js
+++ API/document/src/middleware/redis.js
@@ -1,36 +1,36 @@
-const { createClient } = require("redis");
-const logger = require('../configs/logger');
+const { createClient } = require("redis")
+const logger = require('../configs/logger')
 const crypto = require('crypto')
-const config = require('../configs/config');
+const config = require('../configs/config')
 
 
 
-let redisClient;
+let redisClient
 
 async function initializeRedisClient() {
     // read the Redis connection URL from the envs
-    let redisURL = config.redisURI;
+    let redisURL = config.redisURI
     if (redisURL) {
       // create the Redis client object
       redisClient = createClient({ url: redisURL }).on("error", (e) => {
-        logger.error(`Failed to create the Redis client with error: ${e}`);
-      });
+        logger.error(`Failed to create the Redis client with error: ${e}`)
+      })
       
       try {
         // connect to the Redis server
-        await redisClient.connect();
+        await redisClient.connect()
 
-        logger.info(`Connected to Redis successfully!`);
+        logger.info(`Connected to Redis successfully!`)
       } catch (e) {
-        logger.error(`Connection to Redis failed with error: ${e}`);
+        logger.error(`Connection to Redis failed with error: ${e}`)
 
       }
     }
 }
 
 function requestToKey(id,path) {
-    const reqDataToHash =  id +  path ;
-    return `${path}-${crypto.hash('sha1',reqDataToHash)}`;
+    const reqDataToHash =  id +  path 
+    return `${path}-${crypto.hash('sha1',reqDataToHash)}`
   }
 
 async function writeData(key, data, options) {
@@ -38,69 +38,68 @@ async function writeData(key, data, options) {
         logger.info("Writing data to cache")
         // write data to the Redis cache with options
         if (options != undefined)
-                return await redisClient.set(key, data, options);
+                return await redisClient.set(key, data, options)
             
         // write data to the Redis cache without options
-        return await redisClient.set(key, data);
+        return await redisClient.set(key, data)
         
     } catch (e) {
-    logger.error(`Failed to cache data for key=${key}`, e);
+    logger.error(`Failed to cache data for key=${key}`, e)
     }
 
 }
 
 async function readData(key) {
-    let cachedValue = undefined;
-  
-      // try to get the cached response from redis
-      cachedValue = await redisClient.get(key);
-      if (cachedValue) {
-          return cachedValue;
-      }
+    let cachedValue = undefined
+
+    // try to get the cached response from redis
+    cachedValue = await redisClient.get(key)
+    if (cachedValue) 
+        return cachedValue
 }
 
 async function invalidateCache(key) {
         try {
-            await redisClient.del(key);
-            //logger.info(`Cache invalidated for key ${key}`);
+            await redisClient.del(key)
+            //logger.info(`Cache invalidated for key ${key}`)
         } catch (e) {
-            logger.error(`Failed to invalidate cache for key=${key}`, e);
+            logger.error(`Failed to invalidate cache for key=${key}`, e)
         }
 }
 
 function redisCacheMiddleware(options) {
     return async (req, res, next) => {
-            const key = requestToKey(req.user.coffer_id,req.path);
-            const cachedValue = await readData(key);
+            const key = requestToKey(req.user.coffer_id,req.path)
+            const cachedValue = await readData(key)
 
             if (cachedValue) {
-                logger.info(`Cache hit\n\nCache key: ${key}`);
+                logger.info(`Cache hit\n\nCache key: ${key}`)
                 try {
-                    return res.json(JSON.parse(cachedValue));
+                    return res.json(JSON.parse(cachedValue))
                 } catch (err) {
-                    logger.error(`Failed to parse cached data for key=${key}`, err);
+                    logger.error(`Failed to parse cached data for key=${key}`, err)
                     // Proceed without cache if parsing fails
-                    next();
+                    next()
                 }
             } else {
-                logger.info("Cache miss, proceeding to DB...");
-                const resData = res.json;
+                logger.info("Cache miss, proceeding to DB...")
+                const resData = res.json
 
                 res.json = function (data) {
-                    res.json = resData;
+                    res.json = resData
                     
                     if (res.statusCode == 200) {
-                        logger.info("Caching response...\n");
-                        const dataString = JSON.stringify(data);
-                        writeData(key, dataString, options); 
+                        logger.info("Caching response...\n")
+                        const dataString = JSON.stringify(data)
+                        writeData(key, dataString, options) 
                     }
 
-                    return res.json(data);
-                };
-                next(); // Proceed to the next middleware or route handler
+                    return res.json(data)
+                }
+                next() // Proceed to the next middleware or route handler
             }
          
-    };
+    }
 }
 
 
--- API/document/src/route.js
+++ API/document/src/route.js
@@ -15,9 +15,9 @@ router.use(authjwt)
 
 
 // Personal Documents of Citizenship
-router.get('/personal/:cat',   redisCacheMiddleware(), con_citizenships, control.getCitzDocuments );
-router.get('/personal/tagged/:cat/:tag',redisCacheMiddleware(), con_citizenships, control.getTagDocuments);
-router.get('/personal/counts/:cat', redisCacheMiddleware(), con_citizenships, control.getTagsCount);
+router.get('/personal/:cat',   redisCacheMiddleware({EX:3600}), con_citizenships, control.getCitzDocuments );
+router.get('/personal/tagged/:cat/:tag',redisCacheMiddleware({EX:3600}), con_citizenships, control.getTagDocuments);
+router.get('/personal/counts/:cat', redisCacheMiddleware({EX:3600}), con_citizenships, control.getTagsCount);
 
 // Personal document operations 
 router.post('/personal/:cat/add',  con_citizenships, upload.single('file'), validate_payload(personalDocument_Post), control.postDocument); 
@@ -25,7 +25,7 @@ router.patch('/personal/:cat/:id/update',  IsUser, con_citizenships, validate_pa
 router.delete('/personal/:cat/:id/delete', IsUser, con_citizenships, control.deleteDocument); 
 router.get('/personal/:cat/:id/download', IsUser, redisCacheMiddleware({EX:3540}),  con_citizenships, control.downloadDocument); 
 router.get('/personal/:cat/:id/view', IsUser, redisCacheMiddleware({EX:3540}), con_citizenships, control.viewDocument); 
-router.get('/personal/:cat/:id/details', IsUser, redisCacheMiddleware(), con_citizenships, control.documentDetails); 
+router.get('/personal/:cat/:id/details', IsUser, redisCacheMiddleware({EX:3600}), con_citizenships, control.documentDetails); 
 
 router.post('/p-docs', validate_payload(missingIds), control.get_pdocs);
 router.post('/p-docs/details', control.get_pdocsDetails)
