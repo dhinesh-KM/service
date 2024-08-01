32d6add85e4714e349fd41f488a800f49a617569
dhinesh-KM <dhineshmuthu2002@gmail.com>
2024-08-01 21:20:06 +0530

    gateway changed

--- API/my-gateway/server.js
+++ API/my-gateway/server.js
@@ -1,7 +1,12 @@
 const path = require('path');
 const gateway = require('express-gateway');
+const dotenv = require('dotenv')
 
-console.log("******",process.env.NODE_ENV,process.env.DOCUMENT_HOST, process.env.CONSUMER_HOST,process.env.CONSUMER_SERVICE_URL)
+
+const env = process.env.NODE_ENV || 'development'
+dotenv.config({ path: `.env.${env}`})
+
+
 gateway()
   .load(path.join(__dirname, 'config'))
   .run();
