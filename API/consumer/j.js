0e2c4e3081047be38a00964c34e7d45434add8ff
dhinesh-KM <dhineshmuthu2002@gmail.com>
2024-08-02 14:52:04 +0530

    gateway

--- API/consumer/j.js
+++ API/consumer/j.js
@@ -1,3 +1,17 @@
+CONSUMER_SERVICE_URL=http://127.0.0.1:8000
+DOCUMENT_SERVICE_URL=http://127.0.0.1:6000
+CORS=https://editor.swagger.io
+
+
+CONSUMER_SERVICE_URL=https://consumer-service-l4xd.onrender.com
+DOCUMENT_SERVICE_URL=https://document-service-d6nf.onrender.com
+CORS=https://editor.swagger.io
+
+
+const env = process.env.NODE_ENV || 'development'
+dotenv.config({ path: `.env.${env}`})
+console.log(process.env.CONSUMER_SERVICE_URL, process.env.DOCUMENT_SERVICE_URL, process.env.CORS)
+
 http:
     port: 8080
     cors:
