a0170d45e18a0b6fbcfb06df89d00ae38b74595d
dhinesh-KM <dhineshmuthu2002@gmail.com>
2024-08-02 12:25:23 +0530

    gate

--- API/my-gateway/server.js
+++ API/my-gateway/server.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv')
 
 const env = process.env.NODE_ENV || 'development'
 dotenv.config({ path: `.env.${env}`})
-
+console.log(process.env.CONSUMER_SERVICE_URL, process.env.DOCUMENT_SERVICE_URL)
 gateway()
   .load(path.join(__dirname, 'config'))
   .run();
