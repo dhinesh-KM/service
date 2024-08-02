b677b1f58459352c64666ba5de662cfab9905bf0
dhinesh-KM <dhineshmuthu2002@gmail.com>
2024-08-02 09:36:46 +0530

    server.js updated

--- API/my-gateway/server.js
+++ API/my-gateway/server.js
@@ -6,7 +6,6 @@ const dotenv = require('dotenv')
 const env = process.env.NODE_ENV || 'development'
 dotenv.config({ path: `.env.${env}`})
 
-
 gateway()
   .load(path.join(__dirname, 'config'))
   .run();
